8f35dfe4a466ef94907fd936dc4e052ecb1f051e
Bailey Shelton <104048251+bcshelto565@users.noreply.github.com>
2022-09-20 14:01:58 -0500

    Add files via upload

---
+++ Javascript-stuff/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          No you idiot, there's no <code>CODE</code> here yet. Come back later loser.
+        </p>
+        <a
+          className="App-link"
+          href="https://www.youtube.com/watch?v=o-YBDTqX_ZU"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          NOT WORKING YET, NO CLICKS, THANKS.
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
