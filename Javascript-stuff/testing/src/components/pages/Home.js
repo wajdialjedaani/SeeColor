57e33cde9c5839ebd79b8faa5f43228f9d247bf0
Bailey Shelton <104048251+bcshelto565@users.noreply.github.com>
2022-09-22 00:18:55 -0500

    Re-uploaded testing local files to github inside of testing folder
    Missing node-modules, this will need to be added on the actual site side since this is stuff installed using npm.

---
+++ Javascript-stuff/testing/src/components/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../App.css';
+import MainSection from '../MainSection';
+
+function Home () {
+    // console.log('Home page loaded');
+    return (
+        <>
+            <MainSection />
+        </>
+    )
+}
+
+export default Home;
