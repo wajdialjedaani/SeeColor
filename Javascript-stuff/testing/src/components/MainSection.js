87be3d42bd3472c590a88bfc3ac249fbc449387b
Bailey Shelton <104048251+bcshelto565@users.noreply.github.com>
2022-09-22 14:23:28 -0500

    Add files via upload

--- Javascript-stuff/testing/src/components/MainSection.js
+++ Javascript-stuff/testing/src/components/MainSection.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../App.css';
 import { Button } from './Button';
+import { GetStartedButton } from './GetStartedButton';
 import './MainSection.css';
 import video from '../Videos/synthwave.mp4'
 
@@ -8,26 +9,30 @@ function MainSection() {
     // console.log('test main section');
     return (
     <div className='main-container'>
-        
             <video src={video} type="video/mp4" autoPlay loop muted />
             <h1>HOW ACCESSIBLE IS YOUR COLOR?</h1>
             <p>Let's find out.</p>
+            
             <div className='main-btns'>
-                <Button 
-                    className='btns' 
-                    buttonStyle='btn--outline'
-                    buttonSize='btn--large'>
-                    Get Started
-                </Button>
                 <Button 
                     className='btns' 
                     buttonStyle='btn--primary'
                     buttonSize='btn--large'>
-                    The Project Guidelines <i className='far fa-play-circle' />
+                    The Project Guidelines <i className='far' />
                 </Button>
-            
-        </div>
+            </div>
+            <div className='get-started'>
+                <GetStartedButton
+                    className='btn-center' 
+                    buttonStyle='btn--outline'
+                    buttonSize='btn--large'
+                    align-items='center'
+                    justify-content='center'>
+                    Get Started <br></br>
+                </GetStartedButton>
+            </div>
     </div>
+    
   );
 }
 
