2e555a4eabbc5a01db10c199ada685a793f36841
Sheik13 <102391877+Sheik13@users.noreply.github.com>
2022-10-16 16:24:51 -0500

    Update main.js
    Can now get colour data from a section of canvas

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 
 //image upload
+
 let imgInput = document.getElementById('testImage');
   imgInput.addEventListener('change', function(e) {
     if(e.target.files) {
@@ -14,7 +15,7 @@ let imgInput = document.getElementById('testImage');
           var myContext = myCanvas.getContext("2d"); // Creates a contect object
           myCanvas.width = myImage.width; // Assigns image's width to canvas
           myCanvas.height = myImage.height; // Assigns image's height to canvas
-          myContext.drawImage(myImage,0,0, 720, 480); // Draws the image on canvas
+          myContext.drawImage(myImage,0,0, 400, 300); // Draws the image on canvas
           let imgData = myCanvas.toDataURL("image/jpeg",0.75); // Assigns image base64 string in jpeg format to a variable
         }
       }
@@ -22,3 +23,23 @@ let imgInput = document.getElementById('testImage');
     }
   });
 
+
+  function drawImage() {
+    var canvas = document.getElementById('testCanvas');
+    var context = canvas.getContext('2d');
+
+    var imageData = context.getImageData(10, 10, 50, 50);
+    var data = imageData.data;
+
+    // iterate over all pixels
+    for(var i = 0, n = data.length; i < n; i += 4) {
+      var red = data[i];
+      var green = data[i + 1];
+      var blue = data[i + 2];
+      var alpha = data[i + 3];
+      alert(red + " " + green + " " + blue + " " + alpha);
+    }
+   
+  }
+
+  document.getElementById('testButton').addEventListener('click', drawImage);
