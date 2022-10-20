15695a7f1a35d17423872bd8e0f1512d8f27e9e8
Sheik13 <102391877+Sheik13@users.noreply.github.com>
2022-10-20 15:28:21 -0500

    Now tests colours of image to WCAG standards
    Gets image data of every pixel, and tests all the different colours against each other by calculating their luminance, and then their contrast

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
-
 //image upload
-
 let imgInput = document.getElementById('testImage');
   imgInput.addEventListener('change', function(e) {
     if(e.target.files) {
@@ -16,7 +14,7 @@ let imgInput = document.getElementById('testImage');
           myCanvas.width = myImage.width; // Assigns image's width to canvas
           myCanvas.height = myImage.height; // Assigns image's height to canvas
           myContext.drawImage(myImage,0,0, 400, 300); // Draws the image on canvas
-          let imgData = myCanvas.toDataURL("image/jpeg",0.75); // Assigns image base64 string in jpeg format to a variable
+          let imgData = myCanvas.toDataURL("image/png",0.75); // Assigns image base64 string in jpeg format to a variable
         }
       }
 
@@ -42,12 +40,13 @@ let imgInput = document.getElementById('testImage');
 
     for (var j = 0; j < data.length; j += 4){
       if (red == data[j] && green == data[j+1] && blue == data[j+2] ){
-        alert("same");
+        //alert("same");
         //colours are the same, dont sent to test
       }
       else {
-        alert("not same" + red + " " + green + " " + blue + " " + data[j] + " " + data[j+1] + " " + data[j+2]);
+        //alert("not same" + red + " " + green + " " + blue + " " + data[j] + " " + data[j+1] + " " + data[j+2]);
         //colours are not the same, send to test
+        testTwoColours(red, green, blue, data[j], data[j+1], data[j+2] );
       }
      }
 
@@ -55,3 +54,40 @@ let imgInput = document.getElementById('testImage');
   }
 
   document.getElementById('testButton').addEventListener('click', getImageData);
+
+  //from
+  //https://dev.to/alvaromontoro/building-your-own-color-contrast-checker-4j7o
+  //returns luminance of rgb colour
+  //for contrast calculation
+  function luminance(r, g, b) {
+    var a = [r, g, b].map(function (v) {
+        v /= 255;
+        return v <= 0.03928
+            ? v / 12.92
+            : Math.pow( (v + 0.055) / 1.055, 2.4 );
+    });
+    return a[0] * 0.2126 + a[1] * 0.7152 + a[2] * 0.0722;
+}
+
+
+//uses luminace to calculate contrast of two colours
+//according to WCAG standards
+//displays results of WCAG tests as alert for now
+function testTwoColours(r1, g1, b1, r2, g2, b2){
+
+  const lum1 = luminance(r1, g1, b1);
+  const lum2 = luminance(r2, b2, g2);
+
+  const ratio = lum1 > lum2 
+    ? ((lum2 + 0.05) / (lum1 + 0.05))
+    : ((lum1 + 0.05) / (lum2 + 0.05));
+
+    const result = `
+                AA-level large text: ${ratio < 1/3 ? 'PASS' : 'FAIL' }
+                AA-level small text: ${ratio < 1/4.5 ? 'PASS' : 'FAIL' }
+                AAA-level large text: ${ratio < 1/4.5 ? 'PASS' : 'FAIL' }
+                AAA-level small text: ${ratio < 1/7 ? 'PASS' : 'FAIL' }`;
+
+ 
+  alert(result);
+}
