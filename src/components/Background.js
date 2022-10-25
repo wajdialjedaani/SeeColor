4f5d8077dadb3a0ac243fb61b72e6584299e020e
Bailey Shelton <104048251+bcshelto565@users.noreply.github.com>
2022-10-25 15:43:40 -0500

    Update Background.js

--- src/components/Background.js
+++ src/components/Background.js
@@ -2,7 +2,7 @@ import React from 'react'
 import '../App.css';
 // import { Button } from './Button';
 import './Background.css';
-import image from '../images/img-1.jpg';
+import image from "https://www.salesforce.org/wp-content/uploads/2017/02/University-of-North-Texas-Campus.jpg";
 
 function Background() {
     // console.log('test main section');
