ea92692839c7bba2205f5e42042b1fe245694eb5
Bailey Shelton <104048251+bcshelto565@users.noreply.github.com>
2022-10-25 15:28:37 -0500

    Update Background.js

--- src/components/Background.js
+++ src/components/Background.js
@@ -7,7 +7,7 @@ function Background() {
     // console.log('test main section');
     return (
     <div className='background-container'>
-            <img src="https://www.salesforce.org/wp-content/uploads/2017/02/University-of-North-Texas-Campus.jpg" style="z-index: -1">
+            <img src="https://www.salesforce.org/wp-content/uploads/2017/02/University-of-North-Texas-Campus.jpg" style="z-index: -1"/>
             <h1>What our project is about:</h1>
             <p>The goal of this project is to provide a solution to an issue with media such as posters, images, and graphics being inaccessible to colour blind people. A team member’s parent and friend are color blind and we were inspired by the demonstrated project on poster accessibility. We decided to improve upon it, providing a quick solution to checking the color blind accessibility to graphics, posters, and other media. </p>
             <p>This is our implementation of a web application where all can upload an image and our application will scan it and report how accessible the image is for different types of color-blindess, based on user testing. Our application will give the image a rating for Monochromacy and the three variations of Dichromacy. It will also explain how to improve the ratings for each, and link to background information on colour blindness. </p>
