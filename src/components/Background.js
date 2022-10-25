1ad0decdb13212614220e52317500d8243f0e71d
Bailey Shelton <104048251+bcshelto565@users.noreply.github.com>
2022-10-25 00:45:03 -0500

    Update Background.js

--- src/components/Background.js
+++ src/components/Background.js
@@ -8,7 +8,7 @@ function Background() {
     // console.log('test main section');
     return (
     <div className='background-container'>
-            <image src={image} />
+            <image src="https://www.salesforce.org/wp-content/uploads/2017/02/University-of-North-Texas-Campus.jpg" />
             <h1>What our project is about:</h1>
             <h2>The project guidelines</h2>
             
