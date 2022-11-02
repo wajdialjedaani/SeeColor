934ee4aa7c05a0d1d17c7b16901096a1175141c5
Bailey Shelton <104048251+bcshelto565@users.noreply.github.com>
2022-11-02 17:07:07 -0500

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -28,7 +28,7 @@ function Navbar() {
         <nav className='navbar'>
             <div className='navbar-container'>
                 <Link to="/" className='navbar-logo' onClick={closeMobileMenu}>
-                    SeeColor <i className='fab fa-typeo3' style="margin-right:300px"/>
+                    SeeColor <i className='fab fa-typeo3'/>
                 </Link>
                 <div className='menu-icon' onClick={handleClick}>
                     <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
