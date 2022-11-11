d8ac5169211b5a5f100cfaba79386a765ee296fd
Ricky Gibbons <ricky@Rickys-MacBook-Air.local>
2022-11-11 07:50:59 -0600

    Added font color change support for when the background color is too dark

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -19,20 +19,14 @@ const rgbToHex = (pixel) => {
   return ("#" + componentToHex(pixel.r) + componentToHex(pixel.g) + componentToHex(pixel.b)).toUpperCase();
 };
 
+// determines if the result text should be black or white based on the luminence of the background color
 function textToLight (bgColor, lightColor, darkColor) {
   var color = (bgColor.charAt(0) === '#') ? bgColor.substring(1, 7) : bgColor;
   var r = parseInt(color.substring(0, 2), 16); // hexToR
   var g = parseInt(color.substring(2, 4), 16); // hexToG
   var b = parseInt(color.substring(4, 6), 16); // hexToB
-  var uicolors = [r / 255, g / 255, b / 255];
-  var c = uicolors.map((col) => {
-    if (col <= 0.03928) {
-      return col / 12.92;
-    }
-    return Math.pow((col + 0.055) / 1.055, 2.4);
-  });
-  var L = (0.2126 * c[0]) + (0.7152 * c[1]) + (0.0722 * c[2]);
-  return (L > 0.179) ? darkColor : lightColor;
+  var L = luminance(r, g, b);
+  return (L > 0.179) ? darkColor : lightColor; // 0.179 is from https://www.w3.org/TR/WCAG20/ 
 }
 
 //from https://www.geeksforgeeks.org/program-change-rgb-color-model-hsv-color-model/
