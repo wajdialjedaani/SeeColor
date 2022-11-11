18c0c57fced2172862a47ef3ab14ec016d896e06
Ricky Gibbons <ricky@Rickys-MacBook-Air.local>
2022-11-11 07:38:34 -0600

    Added font color change support for when the background color is too dark

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -19,6 +19,22 @@ const rgbToHex = (pixel) => {
   return ("#" + componentToHex(pixel.r) + componentToHex(pixel.g) + componentToHex(pixel.b)).toUpperCase();
 };
 
+function textToLight (bgColor, lightColor, darkColor) {
+  var color = (bgColor.charAt(0) === '#') ? bgColor.substring(1, 7) : bgColor;
+  var r = parseInt(color.substring(0, 2), 16); // hexToR
+  var g = parseInt(color.substring(2, 4), 16); // hexToG
+  var b = parseInt(color.substring(4, 6), 16); // hexToB
+  var uicolors = [r / 255, g / 255, b / 255];
+  var c = uicolors.map((col) => {
+    if (col <= 0.03928) {
+      return col / 12.92;
+    }
+    return Math.pow((col + 0.055) / 1.055, 2.4);
+  });
+  var L = (0.2126 * c[0]) + (0.7152 * c[1]) + (0.0722 * c[2]);
+  return (L > 0.179) ? darkColor : lightColor;
+}
+
 //from https://www.geeksforgeeks.org/program-change-rgb-color-model-hsv-color-model/
 //returns hue value from an rgb value
 function rgb_to_h(r , g , b) {
@@ -350,7 +366,9 @@ const printPD = (ratios) =>{
     const hex1 = rgbToHex(ratios[i].colour1);
     const hex2= rgbToHex(ratios[i].colour2);
     colour1Element.style.backgroundColor = hex1;
+    colour1Element.style.color = textToLight(hex1, '#F9F9F9', '#000000');
     colour2Element.style.backgroundColor = hex2;
+    colour2Element.style.color = textToLight(hex2, '#F9F9F9', '#000000');
     colour1Element.appendChild(document.createTextNode(hex1));
     colour2Element.appendChild(document.createTextNode(hex2));
 
@@ -432,7 +450,9 @@ const printT = (ratios) =>{
     const hex1 = rgbToHex(ratios[i].colour1);
     const hex2= rgbToHex(ratios[i].colour2);
     colour1Element.style.backgroundColor = hex1;
+    colour1Element.style.color = textToLight(hex1, '#F9F9F9', '#000000');
     colour2Element.style.backgroundColor = hex2;
+    colour2Element.style.color = textToLight(hex2, '#F9F9F9', '#000000');
     colour1Element.appendChild(document.createTextNode(hex1));
     colour2Element.appendChild(document.createTextNode(hex2));
 
@@ -508,7 +528,9 @@ const printContrasts = (ratios) =>{
     const hex1 = rgbToHex(ratios[i].colour1);
     const hex2= rgbToHex(ratios[i].colour2);
     colour1Element.style.backgroundColor = hex1;
+    colour1Element.style.color = textToLight(hex1, '#F9F9F9', '#000000');
     colour2Element.style.backgroundColor = hex2;
+    colour2Element.style.color = textToLight(hex2, '#F9F9F9', '#000000');
     colour1Element.appendChild(document.createTextNode(hex1));
     colour2Element.appendChild(document.createTextNode(hex2));
 
