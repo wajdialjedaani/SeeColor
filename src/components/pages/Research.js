1f500a39ad875d4c433e02908d57a0039f14155c
Ricky Gibbons <ricky@Rickys-MacBook-Air.local>
2022-11-15 14:52:22 -0600

    Small text change

--- src/components/pages/Research.js
+++ src/components/pages/Research.js
@@ -7,7 +7,7 @@ const Reasearch = () => {
 		<div className="research-container">
             <img src = { image } alt="polygons on a off-white background" id="rsrch-img" />
 			<p>Monochromacy: people with this condition cannot distinguish colour at all. This condition is very rare.</p>
-			<p>Dichromacy: People with this condition can tell some colours apart, but are not able to distinguish certain hues. Dichromacy is split into Protanopia (red), Deuteranopia ( green), and Tritanopia (blue). Although each of those are focused mainly on one colours, they all change the way every colour looks, and certain hues can be hard or impossible to tell apart. Protanopia and Deuteranopia cause trouble with reds, greens, and yellows. Tritanopia causes trouble with blues and yellows.</p>
+			<p>Dichromacy: People with this condition can tell some colours apart, but are not able to distinguish certain hues. Dichromacy is split into Protanopia (red), Deuteranopia (green), and Tritanopia (blue). Although each of those are focused mainly on one colours, they all change the way every colour looks, and certain hues can be hard or impossible to tell apart. Protanopia and Deuteranopia cause trouble with reds, greens, and yellows. Tritanopia causes trouble with blues and yellows.</p>
             <p>Anomalous Trichromacy: This condition is split into Tritanomaly, Protanomaly, and Deuteranomaly. Trichromats can still have difficulty distinguishing colour, but not as much as dichromats. This condition can range from very mild to severe. </p>
 			<h3>Sources:</h3>
 			<a href="https://chicago.medicine.uic.edu/departments/academic-departments/ophthalmology-visual-sciences/our-department/media-center/eye-facts/color-blindness/">Chicago Medicine UIC</a>
